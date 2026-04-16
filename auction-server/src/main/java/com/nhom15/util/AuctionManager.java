40ce4c8331d3fd5f5ffe42ab4dd585372e762c64
ThuanDepChai <tranadi222@gmail.com>
2026-04-16 10:04:08 +0700

    Thêm AuctionManager

---
+++ auction-server/src/main/java/com/nhom15/util/AuctionManager.java
@@ -0,0 +1,26 @@
+package com.nhom15.util;
+import com.nhom15.model.auction.Auction;
+import java.util.*;
+
+public class AuctionManager {
+    private static AuctionManager instance;
+    // Danh sach phien dau gia
+    private List<Auction> autions;
+    private AuctionManager(){
+        autions= new ArrayList<>();
+    }
+    public static AuctionManager getInstance(){
+        if (instance == null){
+            instance = new AuctionManager();
+        }
+        return instance;
+    }
+    // Them mot phien dau gia moi
+    public void addAuction(Auction auction){
+        autions.add(auction);
+    }
+    // Lay danh sach dau gia
+    public List<Auction> getAuctions(){
+        return autions;
+    }
+}
