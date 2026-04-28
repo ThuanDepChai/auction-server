66cc9432c67903b5ac2a40b30995f7f03029d9a8
hiepman2007 <hiepman2007@gmail.com>
2026-04-28 08:19:11 +0700

    Cập nhật IP

--- auction-client/src/main/java/com/nhom15/client/network/SocketClient.java
+++ auction-client/src/main/java/com/nhom15/client/network/SocketClient.java
@@ -10,7 +10,7 @@ import java.net.Socket;
 
 public class SocketClient {
 
-    private static final String SERVER_IP = "100.107.11.12";
+    private static final String SERVER_IP = "26.15.123.45";
 
     // Cổng này phải khớp với cổng đang chạy ở AuctionServer
     private static final int SERVER_PORT = 8888;
